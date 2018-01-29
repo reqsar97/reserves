77ef2c56b1e8254c03b19d3ca88d531fb06b35dd
Vigen Arzumanyan <vigen.arzuman@gmail.com>
2018-01-29 21:10:21 +0400

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <link href="//css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="root"></div>
-        <script src="{{asset('js/app.js')}}" ></script>
+        <script src="//js/app.js" ></script>
     </body>
 </html>
