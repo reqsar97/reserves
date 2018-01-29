f493094780fb32c20a1b1a88a469f0b86d2956a3
Vigen Arzumanyan <vigen.arzuman@gmail.com>
2018-01-29 21:35:32 +0400

    wrok on https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (!\App::environment('local')) {
-             URL::forceRootUrl('https://young-chamber-26941.herokuapp.com/');
+             // URL::forceRootUrl('https://young-chamber-26941.herokuapp.com/');
+             $this->app['request']->server->set('HTTPS', true);
              }
 
     }
