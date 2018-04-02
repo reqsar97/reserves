e2584d5ebb24b5f457c7d6b1500ea8f789cbaf6c
Vigen Arzumanyan <vigen.arzuman@gmail.com>
2018-04-03 00:52:10 +0400

    add ch

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Table;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {   
+        User::create([
+            'name'=> 'karen',
+            'email'=> 'karen.tumanyan97@gmail.com',
+            'password'=>'asdasd'
+        ]);
+
         Table::truncate();
         for ($i=1; $i <= 30 ; $i++) {
             # code...
@@ -25,6 +32,13 @@ class DatabaseSeeder extends Seeder
                 "area" => 0,
                 "is_open" => 0
             ]);
+            Table::create([
+                "number" => $i,
+                "area" => 2,
+                "is_open" => 0
+            ]);
+        }
+        for($i=1; $i <= 40; $i++){
             Table::create([
                 "number" => $i,
                 "area" => 2,
